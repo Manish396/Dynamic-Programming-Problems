e65907956864c0cfa07f94b7edf295116ae131de
Manish Kumar <Manish.123jbad@gmail.com>
2021-02-05 14:27:39 +0530

    Update LongestCommonSubsequence.cpp

--- DP_Solutions/LongestCommonSubsequence.cpp
+++ DP_Solutions/LongestCommonSubsequence.cpp
@@ -19,23 +19,20 @@ int main(){
     return 0;
 }
 // function to find longest common subsequence
-int  dp[101][101];
 int lcs(int x, int y, string s1, string s2){
-    
-    // your code here
-    if(x==s1.size() && y==s2.size()){
-        memset(dp, -1, sizeof(dp));
+    int dp[x+1][y+1];
+    for(int i=0; i<=x; i++){
+        for(int j = 0; j <=y; j++){
+            if(i==0 || j==0){
+                dp[i][j] = 0;
+            }
+            else if(s1[i-1] == s2[j-1]){
+                dp[i][j] = dp[i-1][j-1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
     }
-    if(x==0 || y==0){
-        return 0;
-    }
-    if(dp[x][y]!=-1){
-        return dp[x][y];
-    }
-    else if(s1[x-1] == s2[y-1]){
-        return dp[x][y] = 1+lcs(x-1, y-1, s1, s2);
-    }
-    else{
-        return dp[x][y] = max(lcs(x-1, y, s1, s2), lcs(x, y-1, s1, s2));
-    }
+    return dp[x][y];
 }
